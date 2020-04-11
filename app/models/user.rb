178d743cd07e8fe484476ca1a5c2b9fc11b7fbd3
Artem Gapeevtsev <agapeevtsev@yandex.ru>
2020-04-11 12:28:07 +0800

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true, length: { minimum: 2 }
   validates :last_name, presence: true, length: { minimum: 2 }
-  validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)`[a-z]{2,})\z/i }
+  validates :email, presence: true, uniqueness: true, format: { with: /([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}/ }
 end
