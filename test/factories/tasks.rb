2ad9b26f7595d99729bc5906f941782c07ca7b4c
Artem Gapeevtsev <agapeevtsev@yandex.ru>
2020-04-21 16:36:00 +0800

    add factories

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :task do
-    name { "MyString" }
-    description { "MyText" }
+    name { generate :string }
+    description { generate :string }
     author_id { 1 }
     assignee_id { 1 }
-    state { "MyString" }
-    expired_at { "2020-04-09" }
+    state { generate :string }
+    expired_at { '2020-04-09' }
   end
 end
