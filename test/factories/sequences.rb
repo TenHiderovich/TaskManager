b150989710499da4056a2ba95fc7bf504a690187
Artem Gapeevtsev <agapeevtsev@yandex.ru>
2020-05-02 12:52:37 +0800

    update tests

--- test/factories/sequences.rb
+++ test/factories/sequences.rb
@@ -4,7 +4,32 @@ FactoryBot.define do
   sequence :email do |n|
     "email#{n}@factory.com"
   end
-  sequence :string do |n|
-    "string#{n}"
+
+  sequence :first_name do |n|
+    "first_name#{n}"
+  end
+
+  sequence :password do |n|
+    "password#{n}"
+  end
+
+  sequence :last_name do |n|
+    "last_name#{n}"
+  end
+
+  sequence :avatar do |n|
+    "avatar#{n}"
+  end
+
+  sequence :name do |n|
+    "name#{n}"
+  end
+
+  sequence :description do |n|
+    "description#{n}"
+  end
+
+  sequence :expired_at do |n|
+    "expired_at#{n}"
   end
 end
