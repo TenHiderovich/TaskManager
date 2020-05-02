22c5d44d8310586021c05397949361deb6bb286c
Artem Gapeevtsev <agapeevtsev@yandex.ru>
2020-05-02 16:10:44 +0800

    add js imports

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,3 +14,6 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+import 'material-design-lite/material.js';
