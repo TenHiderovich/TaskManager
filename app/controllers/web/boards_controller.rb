0e01e83286866a65cd8af750e52c81b024a1fcfa
Artem Gapeevtsev <agapeevtsev@yandex.ru>
2020-05-15 08:54:04 +0800

    added task board

--- app/controllers/web/boards_controller.rb
+++ app/controllers/web/boards_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class Web::BoardsController < Web::ApplicationController
-  def show; end
+  def show
+    render react_component: 'TaskBoard', props: {}
+  end
 end
