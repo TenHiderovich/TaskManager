59a77789af7b0fcd5bb949c01b55a56713425f2c
Artem Gapeevtsev <agapeevtsev@yandex.ru>
2020-06-02 07:30:32 +0800

    presenters

--- app/javascript/packs/components/Form/Form.js
+++ app/javascript/packs/components/Form/Form.js
@@ -6,6 +6,8 @@ import TextField from '@material-ui/core/TextField';
 
 import UserSelect from '../UserSelect'
 
+import TaskPresenter from 'presenters/TaskPresenter';
+
 import useStyles from './useStyles';
 
 const Form = ({ errors, onChange, task }) => {
@@ -19,7 +21,7 @@ const Form = ({ errors, onChange, task }) => {
         error={has('name', errors)}
         helperText={errors.name}
         onChange={handleChangeTextField('name')}
-        value={task.name}
+        value={TaskPresenter.name(task)}
         label="Name"
         required
         margin="dense"
@@ -28,7 +30,7 @@ const Form = ({ errors, onChange, task }) => {
         error={has('description', errors)}
         helperText={errors.description}
         onChange={handleChangeTextField('description')}
-        value={task.description}
+        value={TaskPresenter.description(task)}
         label="Description"
         required
         multiline
@@ -36,7 +38,7 @@ const Form = ({ errors, onChange, task }) => {
       />
       <UserSelect
         label="Author"
-        value={task.author}
+        value={TaskPresenter.author(task)}
         onChange={handleChangeSelect('author')}
         isRequired
         error={has('author', errors)}
@@ -48,7 +50,7 @@ const Form = ({ errors, onChange, task }) => {
 
 Form.propTypes = {
   onChange: PropTypes.func.isRequired,
-  task: PropTypes.shape().isRequired,
+  task: TaskPresenter.shape().isRequired,
   errors: PropTypes.shape({
     name: PropTypes.arrayOf(PropTypes.string),
     description: PropTypes.arrayOf(PropTypes.string),
