27f3df6cb4200513a1c617d0e1fe345024bc0c6c
Artem Gapeevtsev <agapeevtsev@yandex.ru>
2020-06-02 06:49:10 +0800

    fix: user controller

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -13,19 +13,4 @@ class Api::V1::UsersControllerTest < ActionController::TestCase
     assert_response :success
   end
 
- end
-app/controllers/api/v1/users_controller.rb:
-
-class Api::V1::UsersController < Api::V1::ApplicationController
-  def show
-    user = User.find(params[:id])
-
-    respond_with(user, serializer: UserSerializer)
-  end
-
-  def index
-    users = User.ransack(ransack_params).result.page(page).per(per_page)
-
-    respond_with(users, each_serializer: UserSerializer, meta: build_meta(users), root: 'items')
-  end
 end
