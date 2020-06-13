510747957aaf90b2926421c9367bfbb7a7c3708a
Artem Gapeevtsev <agapeevtsev@yandex.ru>
2020-06-14 07:49:03 +0800

    model methods in file

---
+++ app/models/concerns/token_helper.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+module TokenHelper
+  def new_reset_token
+    self.reset_token = loop do
+      token = SecureRandom.urlsafe_base64
+      break token unless User.exists?(reset_digest: token)
+    end
+  end
+
+  def digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST
+                                                : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def password_reset_expired?
+    time_validity_token = 24
+    reset_sent_at < time_validity_token.hours.ago
+  end
+
+  def create_reset_digest!
+    self.new_reset_token
+    update(:reset_digest =>  digest(reset_token), :reset_sent_at => Time.zone.now)
+  end
+
+  def destroy_reset_digest!
+    self.reset_token = nil
+    update(:reset_digest => nil)
+  end
+
+  def authenticated_reset_token?(token)
+    digest = self.reset_digest
+    return false if digest.nil?
+
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,39 +11,4 @@ class User < ApplicationRecord
   validates :first_name, presence: true, length: { minimum: 2 }
   validates :last_name, presence: true, length: { minimum: 2 }
   validates :email, presence: true, uniqueness: true, format: { with: /@/ }
-
-  def new_reset_token
-    self.reset_token = loop do
-      token = SecureRandom.urlsafe_base64
-      break token unless User.exists?(reset_digest: token)
-    end
-  end
-
-  def digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST
-                                                : BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-  end
-
-  def password_reset_expired?
-    time_validity_token = 24
-    reset_sent_at < time_validity_token.hours.ago
-  end
-
-  def create_reset_digest!
-    self.new_reset_token
-    update(:reset_digest =>  digest(reset_token), :reset_sent_at => Time.zone.now)
-  end
-
-  def destroy_reset_digest!
-    self.reset_token = nil
-    update(:reset_digest => nil)
-  end
-
-  def authenticated_reset_token?(token)
-    digest = self.reset_digest
-    return false if digest.nil?
-
-    BCrypt::Password.new(digest).is_password?(token)
-  end
 end
