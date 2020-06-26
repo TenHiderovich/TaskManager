dc279b01c83331d1a63d474d9cc067160ab2ac5b
Artem Gapeevtsev <agapeevtsev@yandex.ru>
2020-06-26 11:18:01 +0800

    fix: property unique

--- db/migrate/20200607223134_add_reset_to_users.rb
+++ db/migrate/20200607223134_add_reset_to_users.rb
@@ -2,8 +2,8 @@
 
 class AddResetToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :reset_digest, :string
-    add_index  :users, :reset_digest, unique: true
+    add_column :users, :reset_digest, :string, unique: true
+    add_index :users, :reset_digest, unique: true
 
     add_column :users, :reset_sent_at, :datetime
   end
