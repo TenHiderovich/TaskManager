20af351b36178d11a28bc68ce91deb05a72c5481
Artem Gapeevtsev <agapeevtsev@yandex.ru>
2020-07-01 06:28:34 +0800

    fix: remove migration option

--- db/migrate/20200607223134_add_reset_to_users.rb
+++ db/migrate/20200607223134_add_reset_to_users.rb
@@ -2,7 +2,7 @@
 
 class AddResetToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :reset_digest, :string, unique: true
+    add_column :users, :reset_digest, :string
     add_index :users, :reset_digest, unique: true
 
     add_column :users, :reset_sent_at, :datetime
