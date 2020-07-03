7d29f3bd4d752ef9cb8e2afe898807e4e4729202
Artem Gapeevtsev <agapeevtsev@yandex.ru>
2020-07-03 17:15:36 +0800

    fix validate email form

--- app/models/email_form.rb
+++ app/models/email_form.rb
@@ -7,13 +7,10 @@ class EmailForm
     :email
   )
 
-  validates :email, presence: true, format: { with: /@/ }, if: :has_user?
+  validates :email, presence: true, format: { with: /@/ }
+  validates :user, presence: true
 
   def user
     User.find_by(email: email)
-  end
-
-  def has_user?
-    !user.blank?
   end
 end
