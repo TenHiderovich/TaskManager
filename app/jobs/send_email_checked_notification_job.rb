6da2008d77319469e7359d59a4eb3495abd015cd
Artem Gapeevtsev <agapeevtsev@yandex.ru>
2020-07-08 05:36:15 +0800

    fix: deleted the message sending stop

--- app/jobs/send_email_checked_notification_job.rb
+++ app/jobs/send_email_checked_notification_job.rb
@@ -8,7 +8,6 @@ class SendEmailCheckedNotificationJob < ApplicationJob
   def perform(user_id, token)
     user = User.find_by(id: user_id)
     user.reset_token = token
-    return if user.blank?
 
     UserMailer.with(user: user).email_checked.deliver_now
   end
